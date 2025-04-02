77bdf3462966f5b8c60a32eef19d956e3cac7b96
Zulfikar Ditya <52193940+zulfikar-ditya@users.noreply.github.com>
2025-04-02 10:45:56 +0700

    update: resume link

--- app/desktop/desktop.tsx
+++ app/desktop/desktop.tsx
@@ -79,7 +79,7 @@ export default function desktop() {
 									</p>
 									<p className="text-secondaryText">
 										<a
-											href="https://drive.google.com/file/d/1r9tnfhyRF3TDuhbUKAm4XTxPVo8vc41g/view?usp=sharing"
+											href="https://drive.google.com/file/d/1Z-vdKfU7ursp74LSWnjW60NTkTm9gy42/view?usp=sharing"
 											target="_blank"
 											rel="noopener noreferrer"
 											className="flex items-center hover:text-primary dark:hover:text-primary"
