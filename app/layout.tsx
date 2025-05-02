de3b3fb817b730e77970a0460fcb0899e8a0cb9d
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-05-02 17:38:21 +0700

    feat: add experiences and skills data using json, update layout and favicon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,32 +2,61 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Zulfikar Ditya - Backend Developer",
-  description: "Experienced Backend Developer with a demonstrated history of working in the information technology and services industry.",
-  keywords: "backend developer, backend engineer, software engineer, software developer, web developer, web engineer, fullstack developer, fullstack engineer, backend, backend development, backend programming, backend software engineer, backend software developer, backend web developer, backend web engineer, backend fullstack developer, backend fullstack engineer, backend software development, backend web development, backend fullstack development, backend software programming, backend web programming, backend fullstack programming, backend software development engineer, backend web development engineer, backend fullstack development engineer, backend software development developer, backend web development developer, backend fullstack development developer, backend software development programming, backend web development programming, backend fullstack development programming, Zulfikar Ditya, Zulfikar, Ditya",
-  authors: {
-    name: "Zulfikar Ditya",
-    url: "https://github.com/zulfikar4568"
-  },
-  themeColor: "#14b8a6",
-  applicationName: "Zulfikar Ditya",
-  category: "Software Development",
-  robots: "index, follow",
-
+	title: "Zulfikar Ditya - Backend Developer",
+	description:
+		"Experienced Backend Developer with a demonstrated history of working in the information technology and services industry.",
+	keywords:
+		"backend developer, backend engineer, software engineer, software developer, web developer, web engineer, fullstack developer, fullstack engineer, backend, backend development, backend programming, backend software engineer, backend software developer, backend web developer, backend web engineer, backend fullstack developer, backend fullstack engineer, backend software development, backend web development, backend fullstack development, backend software programming, backend web programming, backend fullstack programming, backend software development engineer, backend web development engineer, backend fullstack development engineer, backend software development developer, backend web development developer, backend fullstack development developer, backend software development programming, backend web development programming, backend fullstack development programming, Zulfikar Ditya, Zulfikar, Ditya",
+	authors: {
+		name: "Zulfikar Ditya",
+		url: "https://github.com/zulfikar4568",
+	},
+	themeColor: "#14b8a6",
+	applicationName: "Zulfikar Ditya",
+	category: "Software Development",
+	robots: "index, follow",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
+	return (
+		<html lang="en">
+			<meta
+				content="SjlrZnBfF0NOO11lm4GExHQRf9UM87k4de9teOQxKKc"
+				name="google-site-verification"
+			></meta>
 
-  return (
-    <html lang="en">
-      <meta content="SjlrZnBfF0NOO11lm4GExHQRf9UM87k4de9teOQxKKc" name="google-site-verification"></meta>
-      <body className="bg-backgroundLight dark:bg-backgroundDark">
-        {children}
-      </body>
-    </html>
-  );
+			<link rel="icon" href="favicon/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="favicon/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="favicon/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="favicon/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="favicon/site.webmanifest" />
+			<link
+				rel="mask-icon"
+				href="favicon/safari-pinned-tab.svg"
+				color="#14b8a6"
+			/>
+
+			<body className="bg-backgroundLight dark:bg-backgroundDark">
+				{children}
+			</body>
+		</html>
+	);
 }
